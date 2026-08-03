e1c4408c9e9cf5e6d76863d6315b5e042681a9cf
Drivingh0me <drivingh0me@icloud.com>
2026-08-03 08:44:48 -0500

    making progress on TUI engine

--- src/tui.c
+++ src/tui.c
@@ -11,14 +11,43 @@
 #include <termios.h>
 #include <unistd.h>
 
+typedef struct {
+    int length;
+    /* The len that has been malloc'd */
+    int capacity;
+    char *buffer;
+} Screen;
+
+typedef struct {
+    int rows;
+    int cols;
+} TermSize;
+
+int tui_engine(char c, Screen *screen, TermSize term)
+{
+    screen->length = term.rows * term.cols;
+    screen->buffer = malloc(screen->length * sizeof(char));
+    if (screen->buffer == 0) {
+        return 1;
+    }
+    screen->capacity = screen->length;
+    screen->buffer[0] = c;
+    screen->buffer[1] = c + 1;
+    screen->buffer[2] = 0;
+    return 0;
+}
+
 void run_tui(Toolset tools)
 {
-    int termRows;
-    int termCols;
+    // int termRows;
+    // int termCols;
+    TermSize termsize;
     int tuiShouldRun = 1;
-    char buffer[256];
+    Screen screen;
     char c;
     int err;
+
+    c = ' ';
 
     printf(ALT_SCREEN);
     fflush(stdout);
@@ -35,18 +64,19 @@ void run_tui(Toolset tools)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     /* Vim-like with ':' to start a command */
     while (tuiShouldRun) {
-        TARG_get_term_size(&termRows, &termCols);
+        TARG_get_term_size(&termsize.rows, &termsize.cols);
         printf(CLEAR);
-        fflush(stdout);
         printf(CURSOR_HOME);
-        printf("%c", c);
+
+        err = tui_engine(c, &screen, termsize);
+        printf(screen.buffer);
         fflush(stdout);
         err = read(STDIN_FILENO, &c, 1);
         if (err != 1 || c == 'q') {
             tuiShouldRun = 0;
         }
-
-
+        free(screen.buffer);
+        screen.buffer = NULL;
     }
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig);
     printf(NORM_SCREEN);
