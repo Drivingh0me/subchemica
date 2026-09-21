e4759a4cacb3568782716410003801e998d13cfe
calebgriffin <caleb@azobenzene.tetrahedral.net>
2026-09-20 20:38:28 -0500

    added some math

--- src/compute.c
+++ src/compute.c
@@ -1,44 +1,57 @@
-/* TODO:
-    Could wrap all Vec with 64 bit type into a union with identifier
-    to for a generic Vec type?
-*/
-
 #include "compute.h"
 
 typedef struct {
     double *elements;
-    int length;
+    int size;
     int capacity;
 } VecDouble;
 
 typedef struct {
-    float *elements;
-    int length;
-    int capacity;
-} VecFloat;
-
-typedef struct {
-    long long *elements;
-    int length;
+    int *elements;
+    int size;
     int capacity;
 } VecInt;
 
+/* Maybe make this u8 instead of char? */
+typedef struct {
+    char *elements;
+    int size;
+    int capacity;
+} VecByte;
+
 typedef struct {
     VecDouble matrix;
-    int shape;
+    int rows;
+    int cols;
 } MtxDouble;
+
+typedef struct {
+    VecDouble tens;
+    VecInt shape;
+} TensDouble;
 
 /* Prints a VecDouble in scientific notation */
 int print_vec(VecDouble v) {
     int i = 0;
-    for (i < v.length; i++) {
+    for (i < v.size; i++) {
         printf("%E", v.elements[i]);
     return 0;
 }
 
 /* Append a single value onto a VecDouble. */
-int append_VecDouble(VecDouble v, double d) {
+int append_VecDouble(VecDouble *v, double d) {
+    if (v->size == v->capacity) {
+        /* increase capacity */
+        v->capacity = v->capacity * 2;
+        v->elements = malloc(v->capacity * sizeof(*(v->elements)));
+    }
 
+    if (v->elements == NULL) {
+        v->capacity = 0;
+        v->size = 0;
+        return -1;
+    }
+    return 0;
 }
 
 // #include <gsl/gsl_fit.h>
